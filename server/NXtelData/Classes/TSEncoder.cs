a01964d4927aca8529e5f290e6c647984780764a
Threetwosevensixseven <robin.guest@gmail.com>
2018-11-16 20:21:31 -0500

    Bug fix: telesoftware encoding was resetting the shift mode to |0 at the start of each new frame.

--- server/NXtelData/Classes/TSEncoder.cs
+++ server/NXtelData/Classes/TSEncoder.cs
@@ -23,78 +23,86 @@ namespace NXtelData
             var file = TSFile.Load((int)Page.TeleSoftwareID);
             if (file.FileSizeBytes <= 0)
                 return;
-            Pages = new List<Page>();
-            CurrentPageNo = Page.PageNo;
-            CurrentFrameNo = Page.FrameNo;
-            CurrentEscape = TelesoftEscapes.E0;
-            CurrentPage = Page;
-            Pages.Add(Page);
-            CreateNewPage(); // Add header page for later
-            string fn = (file.FileName ?? "").Trim();
-            if (string.IsNullOrWhiteSpace(fn))
-                fn = "Telesoftware";
-            fn = fn.Replace("|", "|E"); // Escape escape sequence if present in filename
-            string contents = "";
-            CreateNewPage();
-            contents = new string(' ', 40); // Blank line for NXtel header
-            contents += "|A"; // Start of telesoftware block
-            CurrentChecksum = 0;
-            contents += Checksum("|G" + CurrentPage.Frame + "|I"); // Frame letter of telesoftware block, terminated
-            //contents += Checksum("|L"); // EOL, completing header
-            foreach (byte b in file.Contents)
+            //using (var debug = new DebugLogger("NXtel.TSEncode"))
+            DebugLogger debug = null;
             {
-                string newChar = EscapeChar(b);
-                if (contents.Length + newChar.Length > PAGE_LEN)
+                Pages = new List<Page>();
+                CurrentPageNo = Page.PageNo;
+                CurrentFrameNo = Page.FrameNo;
+                CurrentEscape = TelesoftEscapes.E0;
+                CurrentPage = Page;
+                Pages.Add(Page);
+                CreateNewPage(); // Add header page for later
+                string fn = (file.FileName ?? "").Trim();
+                if (string.IsNullOrWhiteSpace(fn))
+                    fn = "Telesoftware";
+                fn = fn.Replace("|", "|E"); // Escape escape sequence if present in filename
+                string contents = "";
+                CreateNewPage();
+                contents = new string(' ', 40); // Blank line for NXtel header
+                contents += "|A"; // Start of telesoftware block
+                CurrentChecksum = 0;
+                contents += Checksum("|G" + CurrentPage.Frame + "|I"); // Frame letter of telesoftware block, terminated
+                                                                       //contents += Checksum("|L"); // EOL, completing header
+                int address = 0;
+                foreach (byte b in file.Contents)
                 {
-                    contents += "|Z" + CurrentChecksum.ToString("D3");
-                    CurrentPage.ConvertContentsFromString(contents);
-                    CreateNewPage();
-                    contents = new string(' ', 40); // Blank line for NXtel header
-                    contents += "|A"; // Start of telesoftware block
-                    CurrentChecksum = 0;
-                    contents += Checksum("|G" + CurrentPage.Frame + "|I"); // Frame letter of telesoftware block, terminated
-                    //contents += Checksum("|L"); // EOL, completing header
-                    contents += Checksum(newChar);
+                    string newChar = EscapeChar(b);
+                    if (contents.Length + newChar.Length > PAGE_LEN)
+                    {
+                        contents += "|Z" + CurrentChecksum.ToString("D3");
+                        CurrentPage.ConvertContentsFromString(contents);
+                        debug.LogWithoutTimestamp("");
+                        CreateNewPage();
+                        //newChar = EscapeChar(b); // Recalculate from TelesoftEscapes.E0
+                        contents = new string(' ', 40); // Blank line for NXtel header
+                        contents += "|A"; // Start of telesoftware block
+                        CurrentChecksum = 0;
+                        contents += Checksum("|G" + CurrentPage.Frame + "|I"); // Frame letter of telesoftware block, terminated
+                                                                               //contents += Checksum("|L"); // EOL, completing header
+                        contents += Checksum(newChar);
+                    }
+                    else
+                    {
+                        contents += Checksum(newChar);
+                    }
+                    debug.Log(address.ToString("X8"), b.ToString("X2") + "  " + b.ToString().PadLeft(3)
+                        + "  " + newChar.PadRight(6) + CurrentEscape.ToString());
+                    address++;
                 }
-                else
-                {
-                    contents += Checksum(newChar);
-                }
-
+                contents += Checksum("|F");
+                contents += "|Z" + CurrentChecksum.ToString("D3"); // end of frame plus checksum
+                CurrentPage.ConvertContentsFromString(contents);
+                // Calculate header contents
+                contents = new string(' ', 40); // Blank line for NXtel header
+                contents += "|A"; // Start of telesoftware block
+                CurrentChecksum = 0;
+                contents += Checksum("|G" + Pages[1].Frame); // Frame letter of telesoftware block
+                contents += Checksum("|I" + fn + "|L"); // Telesoftware filename
+                contents += Checksum((Pages.Count - 2).ToString("D3")); // header frame count
+                contents += "|Z"; // end of telesoftware block
+                contents += CurrentChecksum.ToString("D3"); // header checksum
+                Pages[1].ConvertContentsFromString(contents);
+                Page.PageRange = new NXtelData.Pages();
+                Page.PageRange.AddRange(Pages);
+                var lastPage = Pages[Pages.Count - 1];
+                Pages[0].ToPageNo = lastPage.PageNo;
+                Pages[0].ToFrameNo = lastPage.FrameNo;
+                Pages[0].Routing.AddOrUpdate((byte)RouteKeys.Enter, Pages[0].NextPageNo, Pages[0].NextFrameNo);
+                PageCache.Add(Pages[0]);
             }
-            contents += Checksum("|F");
-            contents += "|Z" + CurrentChecksum.ToString("D3"); // end of frame plus checksum
-            CurrentPage.ConvertContentsFromString(contents);
-            // Calculate header contents
-            contents = new string(' ', 40); // Blank line for NXtel header
-            contents += "|A"; // Start of telesoftware block
-            CurrentChecksum = 0;
-            contents += Checksum("|G" + Pages[1].Frame); // Frame letter of telesoftware block
-            contents += Checksum("|I" + fn + "|L"); // Telesoftware filename
-            contents += Checksum((Pages.Count - 2).ToString("D3")); // header frame count
-            contents += "|Z"; // end of telesoftware block
-            contents += CurrentChecksum.ToString("D3"); // header checksum
-            Pages[1].ConvertContentsFromString(contents);
-            Page.PageRange = new NXtelData.Pages();
-            Page.PageRange.AddRange(Pages);
-            var lastPage = Pages[Pages.Count - 1];
-            Pages[0].ToPageNo = lastPage.PageNo;
-            Pages[0].ToFrameNo = lastPage.FrameNo;
-            Pages[0].Routing.AddOrUpdate((byte)RouteKeys.Enter, Pages[0].NextPageNo, Pages[0].NextFrameNo);
-            PageCache.Add(Pages[0]);
         }
 
-        private void CreateNewPage()
+        private void CreateNewPage(DebugLogger debug = null)
         {
             var page = new Page();
             page.PageNo = CurrentPage.NextPageNo;
             page.FrameNo = CurrentPage.NextFrameNo;
             CurrentPage = page;
             Pages.Add(CurrentPage);
-            CurrentEscape = TelesoftEscapes.E0;
         }
 
-        private string EscapeChar(byte Byte, bool NoEscaping = false)
+        private string EscapeChar(byte Byte, bool NoEscaping = false, DebugLogger debug = null)
         {
             string rv = "";
             if (!NoEscaping)
@@ -107,7 +115,7 @@ namespace NXtelData
                     return EscapeChar(Convert.ToByte('|'), true) + EscapeChar(Convert.ToByte('}'), true);
             }
 
-            if (Byte >= 0 && Byte <= 31)
+            if (Byte >= 0 && Byte <= 31) // 00..1F
             {
                 if (CurrentEscape != TelesoftEscapes.E1)
                 {
@@ -116,7 +124,7 @@ namespace NXtelData
                 }
                 rv += Convert.ToChar(Convert.ToByte(Convert.ToInt32(Byte) + 64));
             }
-            else if (Byte >= 32 && Byte <= 127)
+            else if (Byte >= 32 && Byte <= 127) // 20..7F
             {
                 if (CurrentEscape != TelesoftEscapes.E0)
                 {
@@ -125,7 +133,7 @@ namespace NXtelData
                 }
                 rv += Convert.ToChar(Byte);
             }
-            else if (Byte >= 128 && Byte <= 159)
+            else if (Byte >= 128 && Byte <= 159) // 80..9F
             {
                 if (CurrentEscape != TelesoftEscapes.E2)
                 {
@@ -134,7 +142,7 @@ namespace NXtelData
                 }
                 rv += Convert.ToChar(Convert.ToByte(Convert.ToInt32(Byte) - 64));
             }
-            else if (Byte >= 160 && Byte <= 191)
+            else if (Byte >= 160 && Byte <= 191) // A0..BF
             {
                 if (CurrentEscape != TelesoftEscapes.E3)
                 {
@@ -143,7 +151,7 @@ namespace NXtelData
                 }
                 rv += Convert.ToChar(Convert.ToByte(Convert.ToInt32(Byte) - 96));
             }
-            else if (Byte >= 192 && Byte <= 223)
+            else if (Byte >= 192 && Byte <= 223) // C0..DF
             {
                 if (CurrentEscape != TelesoftEscapes.E4)
                 {
@@ -152,7 +160,7 @@ namespace NXtelData
                 }
                 rv += Convert.ToChar(Convert.ToByte(Convert.ToInt32(Byte) - 128));
             }
-            else if (Byte >= 224 && Byte <= 255)
+            else if (Byte >= 224 && Byte <= 255) // E0..FF
             {
                 if (CurrentEscape != TelesoftEscapes.E5)
                 {
--- server/NXtelServer/Classes/Version.cs
+++ server/NXtelServer/Classes/Version.cs
@@ -2,7 +2,7 @@ namespace NXtelServer
 {
     public static class Version
     {
-        public const string Number = "1.1.0.108";
+        public const string Number = "1.1.0.111";
     }
 }
 
